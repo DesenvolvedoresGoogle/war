a20e61498e66443c143854701427303a0ab8de9e
Gabriel Neutzling <gabrielneutzling@gmail.com>
2014-05-16 16:37:22 -0300

    possibilities object add

--- client/js/application.js
+++ client/js/application.js
@@ -1,3 +1,32 @@
+var possibilities = {
+    AC: [ "AM", "RO" ],
+    AM: [ "AC", "RO", "RR", "PA", "MT" ],
+    AL: [ "SE", "PE", "BH" ],
+    AP: [ "PA" ],
+    BA: [ "SE", "AL", "PE", "PI", "TO", "GO", "MG", "ES" ],
+    CE: [ "RN", "PI", "PB", "PE" ],
+    ES: [ "BA", "MG", "RJ" ],
+    GO: [ "MS", "MT", "TO", "BA" ],
+    MA: [ "PI", "TO", "PA" ],
+    MG: [ "SP", "GO", "BA", "ES", "RJ" ],
+    MS: [ "GO", "MG", "SP", "PR" ],
+    MT: [ "RO", "AM", "PA", "TO", "GO", "MS" ],
+    PA: [ "AP", "AM", "RR", "MT", "TO", "MA" ],
+    PB: [ "RN", "CE", "PE" ],
+    PE: [ "SE", "BA", "PI", "CE", "PB" ],
+    PI: [ "CE", "MA", "TO", "BA", "PE" ],
+    PR: [ "MS", "SP", "SC" ],
+    RJ: [ "SP", "MG", "ES" ],
+    RN: [ "CE", "PB" ],
+    RO: [ "AC", "AM", "MT" ],
+    RR: [ "AM", "PA" ],
+    RS: [ "SC" ],
+    SC: [ "PR", "RS" ],
+    SE: [ "AL", "BA" ],
+    SP: [ "MS", "MG", "PR", "RJ" ],
+    TO: [ "MA", "GO", "PA", "PI", "BH", "MT" ]
+};
+
 var Game = function(options) {
     var app = {}, mapSettings = {
         zoom: 4,
@@ -27,8 +56,7 @@ var Game = function(options) {
                 latLng: ev.latLng
             }, function(results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
-                    var country = app.getCountry(results);
-                    console.log(country);
+                    var stateSelected = app.getCountry(results);
                 }
                 if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
                     console.log("ZERO_RESULTS");
