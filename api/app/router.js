3d5b16a87c5c3926cd51e421dddf517203a88bcb
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-05-16 15:21:55 -0300

    Expose create route

--- api/app/router.js
+++ api/app/router.js
@@ -3,5 +3,6 @@ var GamesController = require('./controllers/games');
 var router = express();
 
 router.get('/api/games', GamesController.index);
+router.post('/api/games', GamesController.create);
 
 module.exports = router;
