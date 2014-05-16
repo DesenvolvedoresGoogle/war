40f7db139533361e71b5dc39357d0eeb17720c1f
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-05-16 16:40:09 -0300

    Fix typo

--- api/app/controllers/games.js
+++ api/app/controllers/games.js
@@ -5,7 +5,7 @@ var GamesController = {
   index: function (req, res) {
     Game.find({}, function (err, results) {
       if (err) {
-        console.err(err.message, err.stack);
+        console.error(err.message, err.stack);
         res.json(500, { message: 'Internal server error' });
       } else {
         res.json(results);
