1f2544b9a08e79ee21073542dc07feb1f370c016
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-05-16 15:29:30 -0300

    Add better logs and body parser

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 var express = require('express');
-var router = require('./app/router.js');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var router = require('./app/router.js');
 var app = express();
 
 mongoose.connect('mongodb://localhost/hackathon-war-dev');
 
-app.use(router);
+app.use(bodyParser.urlencoded())
+  .use(logger())
+  .use(router);
 
 app.listen(8080);
+console.log('Listening on port 8080');
